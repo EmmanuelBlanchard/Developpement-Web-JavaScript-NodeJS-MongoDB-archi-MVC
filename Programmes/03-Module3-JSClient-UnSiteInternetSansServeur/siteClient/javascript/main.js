5f5d1bed2c57fc71f691ac28a59e590308e87b4c
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-12 23:27:02 +0100

    03-Module3-JSClient-UnSiteInternetSansServeur-08-AjouterUnLivre-Etape3

--- Programmes/03-Module3-JSClient-UnSiteInternetSansServeur/siteClient/javascript/main.js
+++ Programmes/03-Module3-JSClient-UnSiteInternetSansServeur/siteClient/javascript/main.js
@@ -26,19 +26,36 @@ var livre4 = {
 
 var tableauLivres = [livre1,livre2,livre3,livre4];
 
-
 for(var i = 0 ; i <= tableauLivres.length - 1 ; i++) {
     var livre = document.createElement("tr");
 
-    livre.innerHTML = `<td>${tableauLivres[i].nom}</td>
-            <td>${tableauLivres[i].auteur}</td>
-            <td>${tableauLivres[i].nombreDePages}</td>
-            <td><button type="button" class="btn btn-warning m-2">Modifier</button></td>
-            <td><button type="button" class="btn btn-danger m-2">Supprimer</button></td>`;
-
+    livre.innerHTML = ` <td>${tableauLivres[i].nom}</td>
+                        <td>${tableauLivres[i].auteur}</td>
+                        <td>${tableauLivres[i].nombreDePages}</td>
+                        <td><button type="button" class="btn btn-warning m-2">Modifier</button></td>
+                        <td><button type="button" class="btn btn-danger m-2">Supprimer</button></td>`;
     table.appendChild(livre);
 }
 
 function AjouterFormulaire() {
     document.querySelector("#ajoutFormulaire").removeAttribute("class");
 }
+
+function ajoutLivre(titre,auteur,nombreDePages) {
+    var livre = {
+        nom : titre,
+        auteur : auteur,
+        nombreDePages : nombreDePages, 
+    }
+    tableauLivres.push(livre);
+    console.log(tableauLivres);
+}
+
+document.querySelector("#validationFormulaireAjout").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var titre = document.querySelector("#ajoutFormulaire #titre").value;
+    var auteur = document.querySelector("#ajoutFormulaire #auteur").value;
+    var nombreDePages = parseInt(document.querySelector("#ajoutFormulaire #nombreDePages").value);
+    ajoutLivre(titre,auteur,nombreDePages);
+});
