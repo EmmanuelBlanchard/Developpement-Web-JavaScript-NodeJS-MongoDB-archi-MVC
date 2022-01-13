5907569034397c9a4b83fc77ea2b9a587b86cd87
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-13 18:12:54 +0100

    04-Module4-LeServeurNodeJS-Express-02-FonctionnementGeneralDuServeur

--- Programmes/04-Module4-LeServeurNodeJS-Express/siteServeur/serveur.js
+++ Programmes/04-Module4-LeServeurNodeJS-Express/siteServeur/serveur.js
@@ -3,7 +3,21 @@ var server= express();
 
 server.listen(3000);
 
-server.use("/", function() {
-    console.log("Reception d'une demande cliente");
-});
+// server.use("/", function() {
+//     console.log("Reception d'une demande cliente");
+// });
 
+server.use("/test", function(requete,reponse) {
+    console.log("Reception d'une demande cliente");
+    console.log("Route de la requête : ");
+    console.log(requete.path);
+    console.log("OriginalUrl de la requête : ");
+    console.log(requete.originalUrl);
+    console.log("ParsedUrl : search de la requête : ");
+    console.log(requete._parsedUrl.search);
+    console.log("Informations sur la requête : ");
+    console.log(requete);
+    console.log("Liste des paramètres reçus : ");
+    console.log(requete.query);
+    reponse.end("Bonjour client : Ici le serveur !");
+});
