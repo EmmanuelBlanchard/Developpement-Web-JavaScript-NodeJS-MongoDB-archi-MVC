8311d047a6c07407770f255b989b79ec6ac8a770
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-13 18:59:59 +0100

    04-Module4-LeServeurNodeJS-Express-05-ModuleMorgan

--- Programmes/04-Module4-LeServeurNodeJS-Express/siteServeur/serveur.js
+++ Programmes/04-Module4-LeServeurNodeJS-Express/siteServeur/serveur.js
@@ -1,26 +1,10 @@
 var express = require("express");
 var server= express();
+var morgan = require("morgan");
+
+server.use(morgan("dev"));
 
 server.listen(3000);
-
-// server.use("/", function() {
-//     console.log("Reception d'une demande cliente");
-// });
-
-// server.use("/test", function(requete,reponse) {
-//     console.log("Reception d'une demande cliente");
-//     console.log("Route de la requête : ");
-//     console.log(requete.path);
-//     console.log("OriginalUrl de la requête : ");
-//     console.log(requete.originalUrl);
-//     console.log("ParsedUrl : search de la requête : ");
-//     console.log(requete._parsedUrl.search);
-//     console.log("Informations sur la requête : ");
-//     console.log(requete);
-//     console.log("Liste des paramètres reçus : ");
-//     console.log(requete.query);
-//     reponse.end("Bonjour client : Ici le serveur !");
-// });
 
 server.get("/", (requete,reponse) => {
     console.log("Page d'accueil demandée");
